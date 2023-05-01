14f212669088984637ff47399ea8e9670ba991c6
AhmedXAlDeeb <ahmedaldiep@gmail.com>
2023-05-02 00:36:53 +0300

    new commit

--- chessGame-Team10/src/main/java/com/example/chessgameteam10/loginView.java
+++ chessGame-Team10/src/main/java/com/example/chessgameteam10/loginView.java
@@ -2,6 +2,7 @@ package com.example.chessgameteam10;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -9,9 +10,14 @@ import java.io.IOException;
 public class loginView extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(gameApp.class.getResource("about.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(gameApp.class.getResource("login.fxml"));
+        loginControl controller = new loginControl();
+        fxmlLoader.setController(controller);
+
+        Parent root = fxmlLoader.load();
+
+        Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
+        stage.setTitle("login and register");
         stage.setScene(scene);
         stage.show();
     }
